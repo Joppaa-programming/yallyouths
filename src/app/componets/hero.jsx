72365d42e46ac00b979b6135a2f7d5879b599588
Joppaa-Main <Joppaaplc@gmail.com>
2024-05-20 02:43:13 +0100

    done gram

--- src/app/componets/hero.jsx
+++ src/app/componets/hero.jsx
@@ -18,11 +18,11 @@ export default function Hero() {
                     </video>
                 </div>
                 {/* overlay */}
-                <div className='opacity-70 w-full h-full bg-black relative'>
+                <div className='opacity-70 w-full h-full bg-black  relative'>
 
                     <div className="container bottom-0 hero-content opacity-100  flex absolute flex-col items-center  ">
                         <div className="w-full mt-6 lg:flex justify-center items-center flex-col  mb-5 sm:mb-10">
-                            <span className='text-5xl sm:text-6xl md:text-6xl lg:text-8xl  text-center  outline-text font-black'>
+                            <span className='text-5xl sm:text-6xl md:text-6xl lg:text-8xl  text-center text-slate-50  font-black'>
                                 <h1 className=" xl:text-6xl lg:text-6xl">
                                     youth
                                 </h1>
@@ -37,7 +37,7 @@ export default function Hero() {
                         <div className="w-full flex mt-2 justify-center items-center">
                             <span className=''>
                                 <a href="#contactus" >
-                                    <button className="hover:bg-yellow-800 hover:p-2 transition duration-150 ease-in-out lg:text-xl lg:font-bold   text-yellow-400 px-4 sm:px-10 border-4 border-yellow-400 py-2 sm:py-4 text-sm"> Donate</button>
+                                    <button className="hover:bg-red-800 hover:p-2 transition duration-150 ease-in-out lg:text-xl lg:font-bold   text-red-400 px-4 sm:px-10 border-4 border-red-400 py-2 sm:py-4 text-sm"> Donate</button>
                                 </a>
                                 <a href="#work" className='mb-3'>
                                     <button className="ml-4 hover:bg-blue-800 hover:p-2 lg:text-xl lg:font-bold  text-blue-400 px-4 sm:px-10 border-4 border-blue-400 py-2 sm:py-4 text-sm">About Us</button>
--- src/app/componets/services.jsx
+++ src/app/componets/services.jsx
@@ -14,7 +14,7 @@ export default function Services() {
           
               
                 <RightService
-                src="https://res.cloudinary.com/dfhcjbewi/image/upload/v1690576370/entrepreneurship-secure-future-africa-youth-key-image_itgopy.png"
+                src="/crowd.jpeg"
                     title="OUR MISSION"
                     description="YALL is a dynamic network of leaders dedicated to raising funds for positive social and environmental change. Through these funds, we empower and support young minds to harness their innovation and entrepreneurship, enabling them to address local challenges with inspiring solutions. Together, we cultivate a brighter future for all."
                     buttonText="JOIN US"
@@ -24,7 +24,7 @@ export default function Services() {
             {/* Social media marketing  */}
         
             <LeftService
-            src="https://res.cloudinary.com/dfhcjbewi/image/upload/v1690584680/impact_yerk36.jpg" 
+            src="/schoolkids.jpeg" 
                 title="OUR IMPACT"
                 description="At YALL, we've already made a positive impact by providing food supplies to several foster homes and orphanages. Now, we're committed to expanding and enhancing our services together. Currently, we support three homes, each housing more than ten children. Our future plans include implementing online classes and educational suites to foster their learning and development further. With your support, we can continue transforming lives and nurturing a brighter future for these deserving children."
                 buttonText="OUR YOUTUBE"
@@ -34,10 +34,10 @@ export default function Services() {
             {/* Search Engine Optimization (SEO) Specialist */}
            
             <LeftService 
-            src="https://res.cloudinary.com/dfhcjbewi/image/upload/v1690584860/vision_egswnz.jpg"
+            src="/youth.jpeg"
                 title="OUR VISION"
                 description="At YALL, our vision is to be a driving force in empowering youth worldwide, transforming every young individual's aspirations into achievements. Through captivating media, an unwavering commitment to positive change, and innovative fundraising, we cultivate a brighter future for all. As a dynamic network of leaders, we aim to create a global movement for youth empowerment, fostering a world where young minds are inspired, supported, and equipped to address local challenges with inspiring solutions. Together, we strive to make a lasting and profound difference in the lives of young people, nurturing a world where their potential knows no bounds."
-                buttonText="BOOK YOUR CONSULTATION"
+                buttonText="Get in touch"
                 link="#contactus"
                 />
          
